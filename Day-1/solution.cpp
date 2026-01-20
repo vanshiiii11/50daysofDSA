6abe9db41e89b2d2f7625cb92d0b93370226a753
Vanshika Motwani <168456793+vanshiiii11@users.noreply.github.com>
2026-01-21 00:31:03 +0530

    Implement binary search in rotated sorted array

---
+++ Day-1/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            // Case 1: duplicates, cannot decide
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            // Case 2: left half sorted
+            else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            // Case 3: right half sorted
+            else {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
